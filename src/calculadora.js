c6f93654cc0e7ade12079d958b7022ead6a52f61
kx31380 <kauquig@outlook.com>
2026-05-26 08:53:18 +0200

    Create calculadora.js

---
+++ src/calculadora.js
@@ -0,0 +1,8 @@
+function sumar(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error("Ambos argumentos deben ser números");
+    }
+    return a + b;
+}
+
+module.exports = { sumar };
